b8a1fee5eb240440dd431dd92c915944a6d1e6d6
Matheusdois <matheustheusssgamer@gmail.com>
2019-11-28 18:44:57 -0300

    ferdpig

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,18 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+			<li class="ts-label">Principal</li>
+			<li><a href="dashboard" style="font-weight: 500;"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			<li class="ts-label">Usuários</li>
+			<li><a href="cadastrados" style="font-weight: 500;"><i class="fa fa-users"></i> Lista de Cadastrados</a></li>
+			<li><a href="deletados" style="font-weight: 500;"><i class="fa fa-user-times"></i>&nbsp;Usuários Excluídos</a></li>
+			<li><a href="download.php" style="font-weight: 500;"><i class="fa fa-download"></i> &nbsp;Baixar Lista de Usuários</a></li>
+			<li class="ts-label">Outros</li>
+			<li><a href="feedback.php" style="font-weight: 500;"><i class="fa fa-envelope"></i>  &nbsp;Feedback Recebidos</a></li>
+			<li><a href="notificacao" style="font-weight: 500;"><i class="fa fa-bell"></i>  &nbsp;Notificações</a></li>
+			<li><a href="contato" style="font-weight: 500;"><i class="fa fa-phone"></i> &nbsp;Solicitação de Contatos</a></li>
+			
+			</ul>
+		</nav>
+
+		
---
+++ includes/leftbar.php
@@ -0,0 +1,17 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+			<li class="ts-label">Menu</li>
+			<li><a href="perfil"><i class="fa fa-user"></i> &nbsp;Perfil</a>
+			</li>
+			<li><a href="feedback"><i class="fa fa-envelope"></i> &nbsp;Feedback</a>
+			</li>
+			<li><a href="notificacoes"><i class="fa fa-bell"></i> &nbsp;Notificações<sup style="color:red">*</sup></a>
+			</li>
+			<li><a href="mensagens"><i class="fa fa-envelope"></i> &nbsp;Mensagens</a>
+			</li>
+			</ul>
+		
+		</nav>
+
+		
